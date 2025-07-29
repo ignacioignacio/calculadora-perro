7add6c1a4c560dc64f2f98eccf45ec5b6bff24b2
ignacioignacio <ignacioperaltaguillamon@gmail.com>
2025-07-29 19:41:49 -0300

    logo
    logo

--- src/App.js
+++ src/App.js
@@ -242,7 +242,7 @@ export default function App() {
         <div className="bg-[#ede8dc] text-[#2a378d] min-h-screen font-sans flex flex-col p-4 sm:p-6 lg:p-8">
             <header className="w-full max-w-4xl mx-auto mb-8 flex justify-between items-center">
                 <h1 className="text-2xl sm:text-3xl text-left">¿Y cuánto me sale?</h1>
-                <img src="https://placehold.co/150x50/ede8dc/2a378d?text=Tu+Logo" alt="Logo de la agencia" className="h-10 rounded" />
+                <img src="hhttps://i.imgur.com/3V0wUeJ.png" alt="Logo de la agencia" className="h-10 rounded" />
             </header>
             
             <main className="w-full max-w-4xl mx-auto flex-grow">
